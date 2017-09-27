5435a7208823dc1c65beda39ab6845f9f1e180cf
Ben Penaluna <penalunaben@gmail.com>
2017-09-27 12:09:15 +1000

    Add new .Net Standard Class Library

---
+++ StormManager.Standard/Services/LocationService/ILocationService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace StormManager.Standard.Services.LocationService
+{
+    interface ILocationService
+    {
+        
+    }
+}
