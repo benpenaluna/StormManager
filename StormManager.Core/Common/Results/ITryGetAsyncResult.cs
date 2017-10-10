dc6b8a253411c40515b847eed19babc493018044
Ben Penaluna <penalunaben@gmail.com>
2017-10-10 21:51:40 +1100

    Added class to contain async 'TryGet' success and result

---
+++ StormManager.Core/Common/Results/ITryGetAsyncResult.cs
@@ -0,0 +1,8 @@
+﻿namespace StormManager.Core.Common.Results
+{
+    public interface ITryGetAsyncResult<T>
+    {
+        bool Success { get; }
+        T Result { get; }
+    }
+}
---
+++ StormManager.Core/Common/Results/TryGetAsyncResult.cs
@@ -0,0 +1,14 @@
+﻿namespace StormManager.Core.Common.Results
+{
+    public class TryGetAsyncResult<T> : ITryGetAsyncResult<T>
+    {
+        public bool Success { get; }
+        public T Result { get; }
+
+        public TryGetAsyncResult(bool success, T result)
+        {
+            this.Success = success;
+            this.Result = result;
+        }
+    }
+}
