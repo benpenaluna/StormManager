46c7f49121cb77163d2a535429e81cb91e17e36b
Ben Penaluna <penalunaben@gmail.com>
2017-11-28 22:28:43 +1100

    Add Extension methods to assist with validity checking of Bing Map Key

---
+++ StormManager.UWP/Common/ExtensionMethods/MapKeyExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+
+namespace StormManager.UWP.Common.ExtensionMethods
+{
+    public static class MapKeyExtensions
+    {
+        public static int MapKeyLength()
+        {
+            return 108;
+        }
+
+        public static bool IsValidMapKey(this string key)
+        {
+            return key.Length == MapKeyLength() && key.ToCharArray().Select(c => (int) c).All(i => i >= 33 && i <= 126);
+        }
+    }
+}
