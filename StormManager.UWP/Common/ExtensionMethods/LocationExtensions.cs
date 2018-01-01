5ecccde98c696b02d94acfb3a6959a4260449153
Ben Penaluna <penalunaben@gmail.com>
2018-01-02 06:30:34 +1100

    Catering for altitude reference, which is required for optimal display on the map

--- StormManager.UWP/Common/ExtensionMethods/LocationExtensions.cs
+++ StormManager.UWP/Common/ExtensionMethods/LocationExtensions.cs
@@ -4,9 +4,10 @@ namespace StormManager.UWP.Common.ExtensionMethods
 {
     public static class LocationExtensions
     {
-        public static Geopoint ToGeopoint(this BasicGeoposition position)
+        public static Geopoint ToGeopoint(this BasicGeoposition position, 
+                                          AltitudeReferenceSystem altitudeReferenceSystem = AltitudeReferenceSystem.Terrain)
         {
-            return new Geopoint(position);
+            return new Geopoint(position, altitudeReferenceSystem);
         }
     }
 }
