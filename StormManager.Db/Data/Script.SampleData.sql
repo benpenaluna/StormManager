995c1c84860898e8258525226cfc020f742100ac
Ben Penaluna <penalunaben@gmail.com>
2018-07-20 21:49:37 +1000

    Creating the back-end repo

---
+++ StormManager.Db/Data/Script.SampleData.sql
@@ -0,0 +1,2 @@
+﻿insert into MemberDetails values
+('ses13596', 'Ben', 'Penaluna', 'AssociateMember', 'Active', 'ses13596@members.ses.vic.gov.au');
---
+++ StormManager.Db/Tables/MemberDetails.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[MemberDetails]
+(
+	[MemberId] VARCHAR(8) NOT NULL PRIMARY KEY, 
+    [FirstName] VARCHAR(100) NULL, 
+    [Surname] VARCHAR(100) NULL, 
+    [Position] VARCHAR(50) NULL, 
+    [Status] VARCHAR(50) NULL, 
+    [EmailAddress] VARCHAR(320) NULL
+)
