77397d2cc0fb0be29aff0fdae47245b2dc28a5f8
Ben Penaluna <penalunaben@gmail.com>
2019-02-11 21:00:19 +1100

    Enabled persisted job types to be displayed in the SettingsPage

--- StormManager.UWP/ViewModels/SettingPageViewModel/JobTypesPartViewModel.cs
+++ StormManager.UWP/ViewModels/SettingPageViewModel/JobTypesPartViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.ObjectModel;
 using StormManager.Standard.Models;
 using StormManager.UWP.Cache;
+using StormManager.UWP.Common;
 using StormManager.UWP.Mvvm;
 
 namespace StormManager.UWP.ViewModels.SettingPageViewModel
@@ -9,9 +10,20 @@ namespace StormManager.UWP.ViewModels.SettingPageViewModel
     {
         public ObservableCollection<Models.JobType> JobTypes { get; set; }
 
+        //public RelayCommand SendCommand { get; set; }
+
+        private bool isFlyoutOpen;
+        public bool IsFlyoutOpen
+        {
+            get => isFlyoutOpen;
+            set { this.Set(() => isFlyoutOpen, ref isFlyoutOpen, value); }
+        }
+        
         public JobTypesPartViewModel()
         {
             JobTypes = new ObservableCollection<Models.JobType>(AppCache.JobTypes);
-        }
+
+            //SendCommand = new RelayCommand(() => { IsFlyoutOpen = false; });
+    }
     }
 }
