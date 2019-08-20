2af2f362301d6c8bf9bc2395a78f0f3bed22cb69
Ben Penaluna <penalunaben@gmail.com>
2019-08-20 11:51:28 +1000

    Remove AppCache class from the solution

--- StormManager.UWP/Cache/AppCache.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace StormManager.UWP.Cache
-{
-    public static class AppCache
-    {
-        public static IEnumerable<Models.JobType> JobTypes => App.UnitOfWork.JobTypes.GetAllJobTypes();  // TODO: Remove this, and point application to App.UnitOfWork
-    }
-}
