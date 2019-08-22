a6290b1bd3124e2e4e46fbccc1b21f6557119ee6
Ben Penaluna <penalunaben@gmail.com>
2019-08-22 21:40:42 +1000

    Preparing to Unit Test WebApiService

---
+++ StormManager.UWP.Tests/Services/WebApiService/MockWebApiHelper.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using StormManager.UWP.Common.Exceptions;
+using StormManager.UWP.Services.WebApiService;
+
+namespace StormManager.UWP.Tests.Services.WebApiService
+{
+    public class MockWebApiHelper : IWebApiHelper
+    {
+        private const string JsonJobTypeSample = "[{\"Id\":1,\"Category\":\"Air Observations and Assessment\",\"SubCategory\":\"\",\"IsUsed\":true,\"NewJobArgb\":-9868951,\"AgingJobArgb\":-16777216},{\"Id\":2,\"Category\":\"Aircraft Incident\",\"SubCategory\":\"Aircraft in Difficulty\",\"IsUsed\":true,\"NewJobArgb\":-9868951,\"AgingJobArgb\":-16777216},{\"Id\":3,\"Category\":\"Aircraft Incident\",\"SubCategory\":\"Insufficient Information to Classify\",\"IsUsed\":true,\"NewJobArgb\":-9868951,\"AgingJobArgb\":-16777216}]";
+
+        public bool MockInternetConnectionExists { get; set; }
+
+        public MockWebApiHelper()
+        {
+            MockInternetConnectionExists = true;
+        }
+
+        public async Task<IEnumerable<T>> GetAsync<T>(string storedProcedureName)
+        {
+            if (!MockInternetConnectionExists)
+                throw new InternetConnectionUnavailableException();
+
+            await Task.Delay(2000);
+
+            return JsonConvert.DeserializeObject<IEnumerable<T>>(JsonJobTypeSample);
+        }
+
+        public async Task PutAsync<T>(string storedProcedureName, T payload)
+        {
+            await Task.Delay(400);
+        }
+    }
+}
--- StormManager.UWP.Tests/Services/WebApiService/WebApiServiceTests.cs
+++ StormManager.UWP.Tests/Services/WebApiService/WebApiServiceTests.cs
@@ -1,14 +1,50 @@
-﻿using Xunit;
+﻿using MvvmCross.Binding.ExtensionMethods;
+using StormManager.Standard.Models;
+using StormManager.UWP.Common.Exceptions;
+using Xunit;
+using JobType = StormManager.UWP.Models.JobType;
 
 namespace StormManager.UWP.Tests.Services.WebApiService
 {
     public class WebApiServiceTests
     {
+        private const string ConnectionString = "foo";
+        private const string StoredProcedureName = "bar";
+
         [Fact]
         public void WebApiService_CanInstantiate()
         {
-            //var result = new UWP.Services.WebApiService.WebApiService();
-            //Assert.NotNull(result);
+            var result = new UWP.Services.WebApiService.WebApiService(ConnectionString);
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public void WebApiService_CanInstantiateWithHelper()
+        {
+            var helper = new MockWebApiHelper();
+            var result = new UWP.Services.WebApiService.WebApiService(ConnectionString, helper);
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public void WebApiService_ThrowsExceptionWhenNoConnectivity()
+        {
+            var helper = new MockWebApiHelper { MockInternetConnectionExists=false };
+            var result = new UWP.Services.WebApiService.WebApiService(ConnectionString, helper);
+            Assert.ThrowsAsync<InternetConnectionUnavailableException>(async () => await result.GetAsync<JobType>(StoredProcedureName));
+        }
+
+        [Fact]
+        public async void WebApiService_CanReturnJobTypes()
+        {
+            var helper = new MockWebApiHelper();
+
+            var result = new UWP.Services.WebApiService.WebApiService(ConnectionString, helper);
+
+            var jobTypes = await result.GetAsync<JobType>(StoredProcedureName);
+
+            Assert.NotNull(jobTypes);
+            Assert.True(jobTypes.Count() == 3);
         }
     }
 }
