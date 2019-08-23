9488a416d6776f3e5e13df51970b0342c8d7cd6e
Ben Penaluna <penalunaben@gmail.com>
2019-08-23 21:34:42 +1000

    Updated GetAllJobTypes Stored Procedure to get new columns

---
+++ StormManager.WebService/Migrations/201908231129480_UpdateGetAllJobTypesStoredProcedureForNewColumns.cs
@@ -0,0 +1,40 @@
+namespace StormManager.WebService.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class UpdateGetAllJobTypesStoredProcedureForNewColumns : DbMigration
+    {
+        public override void Up()
+        {
+            DropStoredProcedure("GetAllJobTypes");
+
+            CreateStoredProcedure("GetAllJobTypes",
+                p => new
+                {
+                    jsonOutput = p.String(outParameter: true)
+                },
+                @"SET @jsonOutput = (SELECT Id, Category, SubCategory, IsUsed, NewJobArgb, AgingJobArgb, DateUpdated, UpdatedBy
+                                          FROM dbo.JobTypes 
+                                          ORDER BY Category, SubCategory
+                                          FOR JSON AUTO)"
+            );
+        }
+        
+        public override void Down()
+        {
+            DropStoredProcedure("GetAllJobTypes");
+
+            CreateStoredProcedure("GetAllJobTypes",
+                p => new
+                {
+                    jsonOutput = p.String(outParameter: true)
+                },
+                @"SET @jsonOutput = (SELECT Id, Category, SubCategory, IsUsed, NewJobArgb, AgingJobArgb 
+                                          FROM dbo.JobTypes 
+                                          ORDER BY Category, SubCategory
+                                          FOR JSON AUTO)"
+            );
+        }
+    }
+}
