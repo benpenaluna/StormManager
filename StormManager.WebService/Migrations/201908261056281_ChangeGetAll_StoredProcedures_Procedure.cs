221aea4f3f54d53483a407f33b006173c514282d
Ben Penaluna <penalunaben@gmail.com>
2019-08-26 20:58:14 +1000

    Update GeyAll_StoredProcedures procedure

---
+++ StormManager.WebService/Migrations/201908261056281_ChangeGetAll_StoredProcedures_Procedure.cs
@@ -0,0 +1,38 @@
+namespace StormManager.WebService.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ChangeGetAll_StoredProcedures_Procedure : DbMigration
+    {
+        public override void Up()
+        {
+            DropStoredProcedure("GetAll_StoredProcedures");
+
+            CreateStoredProcedure("GetAll_StoredProcedures",
+                p => new
+                {
+                    jsonOutput = p.String(outParameter: true)
+                },
+                @"SET @jsonOutput = (SELECT * 
+                                         FROM [dbo].[Routines] 
+                                         FOR JSON AUTO)"
+            );
+        }
+        
+        public override void Down()
+        {
+            DropStoredProcedure("GetAll_StoredProcedures");
+
+            CreateStoredProcedure("GetAll_StoredProcedures",
+                p => new
+                {
+                    jsonOutput = p.String(outParameter: true)
+                },
+                @"SET @jsonOutput = (SELECT * 
+                                         FROM [INFORMATION_SCHEMA].[ROUTINES] 
+                                         FOR JSON AUTO)"
+            );
+        }
+    }
+}
