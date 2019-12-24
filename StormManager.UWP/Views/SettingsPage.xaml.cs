84679ea1c179d7e3a3ec38222dfda56f022ab093
Ben Penaluna <penalunaben@gmail.com>
2019-12-24 20:14:58 +1100

    Made JobTypes SplitView OpenPaneLength bound to the ActualWidth of the SplitView

--- StormManager.UWP/Views/SettingsPage.xaml.cs
+++ StormManager.UWP/Views/SettingsPage.xaml.cs
@@ -1,4 +1,4 @@
-using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Navigation;
 
 namespace StormManager.UWP.Views
@@ -6,6 +6,8 @@ namespace StormManager.UWP.Views
     public sealed partial class SettingsPage
     {
         readonly Services.SerializationService.ISerializationService _serializationService;
+
+        private const double JobTypeContentFrameWidth = 450.0D;
 
         public SettingsPage()
         {
@@ -18,6 +20,13 @@ namespace StormManager.UWP.Views
         {
             var index = int.Parse(_serializationService.Deserialize(e.Parameter?.ToString()).ToString());
             MyPivot.SelectedIndex = index;
+        }
+
+        private void JobTypesSplitView_OnSizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            JobTypesSplitView.OpenPaneLength = JobTypesSplitView.ActualWidth > JobTypeContentFrameWidth ?
+                                               JobTypesSplitView.ActualWidth - JobTypeContentFrameWidth :
+                                               0.0D;
         }
     }
 }
