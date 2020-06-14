e098d8d970a3cf02e3ef107c5c714550acbb3782
Ben Penaluna <penalunaben@gmail.com>
2020-06-14 15:08:04 +1000

    Added Key in Parameters Migration

--- StormManager.Standard/Models/InformationSchema/Routine.cs
+++ StormManager.Standard/Models/InformationSchema/Routine.cs
@@ -7,5 +7,6 @@ namespace StormManager.Standard.Models.InformationSchema
         public string TableName { get; set; }
         public string TransactionType { get; set; }
         public string StoredProcedureName { get; set; }
+        public bool KeyInParameters { get; set; }
     }
 }
---
+++ StormManager.WebService/Migrations/202001171018118_AddKeyInParametersColumnToGetAllStoredProcedures.cs
@@ -0,0 +1,18 @@
+namespace StormManager.WebService.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddKeyInParametersColumnToRoutinesTable : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Routines", "KeyInParameters", c => c.Boolean(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Routines", "KeyInParameters");
+        }
+    }
+}
