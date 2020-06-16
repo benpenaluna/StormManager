231dae8cc3f729d1d76f33113d8edc019d6c4f83
Ben Penaluna <penalunaben@gmail.com>
2020-06-17 09:46:43 +1000

    Created Delete_JobTypes Stored Procedure

---
+++ StormManager.WebService/Migrations/202006162337477_CreateDeleteJobTypeStoredProcedure.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Data.Entity.Migrations;
+
+namespace StormManager.WebService.Migrations
+{
+    public partial class CreateDeleteJobTypeStoredProcedure : DbMigration
+    {
+        public override void Up()
+        {
+            CreateStoredProcedure("Delete_JobType", p => new
+            {
+                id = p.Int()
+            },
+            @"DELETE FROM dbo.JobTypes 
+              WHERE Id = @id;");
+        }
+        
+        public override void Down()
+        {
+            DropStoredProcedure("Delete_JobType");
+        }
+    }
+}
