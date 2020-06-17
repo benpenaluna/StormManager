e213bd08fc6e534e5b6566281f6426a2afd5baf4
Ben Penaluna <penalunaben@gmail.com>
2020-06-17 11:50:40 +1000

    Add Delete_JobTypes to the Routine table

---
+++ StormManager.WebService/Migrations/202006170001433_InsertDeleteJobTypesIntoRoutines.cs
@@ -0,0 +1,20 @@
+namespace StormManager.WebService.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InsertDeleteJobTypesIntoRoutines : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("INSERT INTO dbo.Routines (TableName, TransactionType, StoredProcedureName, KeyInParameters) " +
+                "VALUES ('JobType', 'Deletion', 'Delete_JobType', 1)");
+        }
+        
+        public override void Down()
+        {
+            Sql("DELETE FROM dbo.Routines" +
+                "WHERE TableName = 'JobType' AND TransactionType = 'Deletion'");
+        }
+    }
+}
